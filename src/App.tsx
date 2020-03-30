b2556009e4019ea101fb670d8d6de0f6e6bbd57a
mustran <musli.ademi@inplayer.com>
2020-03-30 17:06:34 +0200

    Test Checkbox

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,44 @@ import React, { useState } from "react";
 import Checkbox from "./elements/Checkbox";
 
 function App() {
-    const [checked, setChecked] = useState(false);
-
+    const [checkMe, setCheckMe] = useState(false);
+    const [checkedByDefault, setCheckedByDefault] = useState(true);
+    console.log(checkMe);
     return (
-        <Checkbox
-            id="id"
-            checked={checked}
-            label="Check me!"
-            onChange={e => console.log(e)}
-        />
+        <>
+            <Checkbox
+                id="1"
+                checked={checkMe}
+                label="Check me!"
+                onChange={() => setCheckMe(!checkMe)}
+            />
+            <Checkbox
+                id="2"
+                checked={checkedByDefault}
+                label="Checked by default, but can be changed"
+                onChange={() => setCheckedByDefault(!checkedByDefault)}
+            />
+            <Checkbox
+                id="3"
+                checked
+                label="Checked by default, cannot be changed"
+                onChange={e => console.log(e)}
+            />
+            <Checkbox
+                id="4"
+                checked={false}
+                label="Disabled and unchecked!"
+                onChange={e => console.log(e)}
+                disabled
+            />
+            <Checkbox
+                id="5"
+                checked
+                label="Disabled and checked!"
+                onChange={e => console.log(e)}
+                disabled
+            />
+        </>
     );
 }
 
--- src/elements/Checkbox/index.tsx
+++ src/elements/Checkbox/index.tsx
@@ -1,39 +1,45 @@
-import React, { ChangeEvent, RefObject } from 'react';
-import { Label } from '../../elements/Label';
-import CheckboxWrapper from './CheckboxWrapper';
-import { CSSProperties } from 'styled-components';
+import React, { ChangeEvent, RefObject } from "react";
+import { Label } from "../../elements/Label";
+import CheckboxWrapper from "./CheckboxWrapper";
+import { CSSProperties } from "styled-components";
 
 type Props = {
-  label: string;
-  id: string;
-  name?: string;
-  checked: boolean;
-  onChange: (checked: boolean) => any;
-  containerRef?: RefObject<HTMLDivElement> | null;
-  className?: string;
-  style?: CSSProperties;
-  disabled?: boolean;
+    label: string;
+    id: string;
+    name?: string;
+    checked: boolean;
+    onChange: (checked: boolean) => any;
+    containerRef?: RefObject<HTMLDivElement> | null;
+    className?: string;
+    style?: CSSProperties;
+    disabled?: boolean;
 };
 
 const Checkbox = ({
-  label,
-  id,
-  onChange,
-  containerRef = null,
-  className = '',
-  style = {},
-  disabled = false,
-  ...rest
+    label,
+    id,
+    onChange,
+    containerRef = null,
+    className = "",
+    style = {},
+    disabled = false,
+    ...rest
 }: Props) => {
-  const onCheckboxChange = (e: ChangeEvent<HTMLInputElement>): any =>
-    onChange(e.target.checked);
+    const onCheckboxChange = (e: ChangeEvent<HTMLInputElement>): any =>
+        onChange(e.target.checked);
 
-  return (
-    <CheckboxWrapper className={className} style={style} ref={containerRef}>
-      <input type="checkbox" id={id} onChange={onCheckboxChange} {...rest} />
-      <Label htmlFor={id}>{label}</Label>
-    </CheckboxWrapper>
-  );
+    return (
+        <CheckboxWrapper className={className} style={style} ref={containerRef}>
+            <input
+                disabled={disabled}
+                type="checkbox"
+                id={id}
+                onChange={onCheckboxChange}
+                {...rest}
+            />
+            <Label htmlFor={id}>{label}</Label>
+        </CheckboxWrapper>
+    );
 };
 
 export default Checkbox;
