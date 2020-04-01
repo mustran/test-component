fb3439e0bc7fc603f721b33e2c421e7dddcaf5f3
mustran <musli.ademi@inplayer.com>
2020-04-01 15:54:40 +0200

    Test UserMenu

--- src/App.tsx
+++ src/App.tsx
@@ -1,67 +1,21 @@
-import React, { useState } from "react";
-import { Dropdown } from "./elements/Dropdown";
+import React from "react";
+import { UserMenu } from "./components/UserMenu";
 
 function App() {
-    const options = [
-        { value: "Option 1", displayName: "Option 1" },
-        { value: "Option 2", displayName: "Option 2" }
-    ];
-
-    const [value, setValue] = useState(options[0].value);
-    const defaultOption = { displayName: "default value", disabled: true };
-
     return (
         <>
-            <Dropdown options={options} value={options[0].value} disabled />
-            <br />
-            <Dropdown
-                options={options}
-                value={value}
-                onChange={e => setValue(e.target.value)}
-                color="#936"
+            <UserMenu
+                image="https://miro.medium.com/max/1200/1*mk1-6aYaf_Bes1E3Imhc0A.jpeg"
+                menuTitle="User name"
+                menuItems={[{ title: "Hello" }, { title: "World" }]}
+                actionItem={{ title: "Logout" }}
             />
-            <br />
-            <Dropdown
-                defaultOption={defaultOption}
-                options={options}
-                value={value}
-                onChange={e => setValue(e.target.value)}
-            />
-            <br />
-            <br />
-            <div>
-                <Dropdown
-                    modifiers={["fontSizeExtraSmall"]}
-                    options={options}
-                    value={options[0].value}
-                    onChange={console.log}
-                />
-                <br />
-                <br />
-                <Dropdown
-                    modifiers={["fontSizeSmall"]}
-                    options={options}
-                    value={options[0].value}
-                    onChange={console.log}
-                />
-                <br />
-                <br />
-                <Dropdown
-                    modifiers={["fontSizeMedium"]}
-                    options={options}
-                    value={options[0].value}
-                    onChange={console.log}
-                />
-                <br />
-                <br />
-                <Dropdown
-                    modifiers={["fontSizeLarge"]}
-                    options={options}
-                    value={options[0].value}
-                    onChange={console.log}
+            <div style={{ marginTop: "20%" }}>
+                <UserMenu
+                    menuTitle="User name"
+                    actionItem={{ title: "Logout" }}
                 />
             </div>
-            
         </>
     );
 }
