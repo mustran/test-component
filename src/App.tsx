d2e14de9c3126a966f361ab565237c4c65e031ce
mustran <musli.ademi@inplayer.com>
2020-04-02 11:01:22 +0200

    Test Tabs

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,37 @@
-import React from "react";
-import { UserMenu } from "./components/UserMenu";
+import React, { useState } from "react";
+import Tabs from "./components/Tabs/Tabs";
+import { FaUserCircle, FaUserAlt } from "react-icons/fa";
 
 function App() {
+    const [selectedTab, setSelectedTab] = useState(0);
+
+    const tabs = [
+        {
+            name: "Account",
+            icon: <FaUserCircle />,
+            iconModifiers: "statusColorWarning"
+        },
+        { name: "User", icon: <FaUserAlt />, iconPosition: "right" },
+        { name: "Contact" }
+    ];
+
+    const tabs1 = [{ name: "Account" }, { name: "User" }, { name: "Contact" }];
+
     return (
         <>
-            <UserMenu
-                image="https://miro.medium.com/max/1200/1*mk1-6aYaf_Bes1E3Imhc0A.jpeg"
-                menuTitle="User name"
-                menuItems={[{ title: "Hello" }, { title: "World" }]}
-                actionItem={{ title: "Logout" }}
+            <Tabs
+                tabs={tabs}
+                selectedTabIndex={selectedTab}
+                onTabClick={index => setSelectedTab(index)}
             />
-            <div style={{ marginTop: "20%" }}>
-                <UserMenu
-                    menuTitle="User name"
-                    actionItem={{ title: "Logout" }}
-                />
-            </div>
+            <br />
+            <br />
+            <br />
+            <Tabs
+                tabs={tabs1}
+                selectedTabIndex={0}
+                onTabClick={index => console.log(index)}
+            />
         </>
     );
 }
