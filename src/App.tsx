83b8bf5acd4e57b47b28ce50c9aea6db83085bd8
mustran <musli.ademi@inplayer.com>
2020-04-02 16:34:48 +0200

    Test Pagination

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,16 @@
 import React, { useState } from "react";
-import Tabs from "./components/Tabs/Tabs";
-import { FaUserCircle, FaUserAlt } from "react-icons/fa";
+import Pagination from "./components/Pagination/Pagination";
 
-function App() {
-    const [selectedTab, setSelectedTab] = useState(0);
-
-    const tabs = [
-        {
-            name: "Account",
-            icon: <FaUserCircle />,
-            iconModifiers: "statusColorWarning"
-        },
-        { name: "User", icon: <FaUserAlt />, iconPosition: "right" },
-        { name: "Contact" }
-    ];
-
-    const tabs1 = [{ name: "Account" }, { name: "User" }, { name: "Contact" }];
-
+const App = () => {
     return (
-        <>
-            <Tabs
-                tabs={tabs}
-                selectedTabIndex={selectedTab}
-                onTabClick={index => setSelectedTab(index)}
-            />
-            <br />
-            <br />
-            <br />
-            <Tabs
-                tabs={tabs1}
-                selectedTabIndex={0}
-                onTabClick={index => console.log(index)}
-            />
-        </>
+        <Pagination
+            onPageChange={console.log}
+            totalItems={50}
+            startPage={4}
+            numberOfPagesDisplayed={10}
+            itemsPerPage={12}
+        />
     );
-}
+};
 
 export default App;
