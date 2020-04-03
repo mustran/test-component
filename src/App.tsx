896447a52aad4d4fbe298ff63e48b8ce8c46a537
mustran <musli.ademi@inplayer.com>
2020-04-03 11:25:32 +0200

    Test Tooltip

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,36 @@
-import React, { useState } from "react";
-import Pagination from "./components/Pagination/Pagination";
+import React from "react";
+import { Tooltip } from "./components/Tooltip";
+import { Button } from "./elements/Button";
 
-const App = () => {
-    return (
-        <Pagination
-            onPageChange={console.log}
-            totalItems={50}
-            startPage={4}
-            numberOfPagesDisplayed={10}
-            itemsPerPage={12}
-        />
-    );
-};
+const App = () => (
+    <div style={{ margin: "300px" }}>
+        <Tooltip content="😎">
+            <Button>Hover me!</Button>
+        </Tooltip>
+        <br />
+        <br />
+        <Tooltip content="Tooltip at the left" placement="left" background="red">
+            <Button>Hover me too!</Button>
+        </Tooltip>
+        <br />
+        <br />
+        <Tooltip content="Tooltip at the right" placement="right">
+            <Button>Hover me as well!</Button>
+        </Tooltip>
+        <br />
+        <br />
+        <Tooltip
+            content={`Tooltip at the bottom\nTooltip at the bottom`}
+            placement="bottom"
+        >
+            <Button>Hover me also!</Button>
+        </Tooltip>
+        <br />
+        <br />
+        <Tooltip content="Hello!" behavior="click" placement="right">
+            <Button>Click me!</Button>
+        </Tooltip>
+    </div>
+);
 
 export default App;
