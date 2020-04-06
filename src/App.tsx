dc2a999b7ebe235214ac1bd1fbe1236183f4bfdc
mustran <musli.ademi@inplayer.com>
2020-04-06 11:19:14 +0200

    Test Accordion

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,72 @@
-import React from "react";
+import React, { useState } from "react";
 import { Tooltip } from "./components/Tooltip";
 import { Button } from "./elements/Button";
+import { Accordion } from "./elements/Accordion";
+import { MdInfo } from "react-icons/md";
 
-const App = () => (
-    <div style={{ margin: "300px" }}>
-        <Tooltip content="😎">
-            <Button>Hover me!</Button>
-        </Tooltip>
-        <br />
-        <br />
-        <Tooltip content="Tooltip at the left" placement="left" background="red">
-            <Button>Hover me too!</Button>
-        </Tooltip>
-        <br />
-        <br />
-        <Tooltip content="Tooltip at the right" placement="right">
-            <Button>Hover me as well!</Button>
-        </Tooltip>
-        <br />
-        <br />
-        <Tooltip
-            content={`Tooltip at the bottom\nTooltip at the bottom`}
-            placement="bottom"
-        >
-            <Button>Hover me also!</Button>
-        </Tooltip>
-        <br />
-        <br />
-        <Tooltip content="Hello!" behavior="click" placement="right">
-            <Button>Click me!</Button>
-        </Tooltip>
-    </div>
-);
+const App = () => {
+    const panels = [
+        {
+            label: "Accordion1",
+            icon: <MdInfo color="orange" />,
+            renderContent: () => {
+                return (
+                    <div>
+                        <h1>Long Content for the accordion1</h1>
+                        <h1>Long Content for the accordion1</h1>
+                        <h1>Long Content for the accordion1</h1>
+                        <h1>Long Content for the accordion1</h1>
+                        <h1>Long Content for the accordion1</h1>
+                        <h1>Long Content for the accordion1</h1>
+                    </div>
+                );
+            },
+        },
+        {
+            label: "Disabled Accordion",
+            icon: null,
+            renderContent: () => {
+                return (
+                    <div>
+                        <h1>Long Content for the accordion</h1>
+                        <h1>Long Content for the accordion</h1>
+                        <h1>Long Content for the accordion</h1>
+                        <h1>Long Content for the accordion</h1>
+                        <h1>Long Content for the accordion</h1>
+                        <h1>Long Content for the accordion</h1>
+                    </div>
+                );
+            },
+            disabled: true
+        },
+        {
+            label: "Accordion3",
+            icon: null,
+            renderContent: () => {
+                return (
+                    <div>
+                        <h1>Long Content for the accordion3</h1>
+                        <h1>Long Content for the accordion3</h1>
+                        <h1>Long Content for the accordion3</h1>
+                        <h1>Long Content for the accordion3</h1>
+                        <h1>Long Content for the accordion3</h1>
+                        <h1>Long Content for the accordion3</h1>
+                    </div>
+                );
+            },
+        }
+    ];
+
+    return (
+        <Accordion
+            onActivePanelChange={index => console.log(index)}
+            panels={panels}
+            contentHeight="46vh"
+            isExtendable
+            width="80%"
+            extendWidth="20%"
+        />
+    );
+};
 
 export default App;
